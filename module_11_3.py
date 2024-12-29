f2faffc0ca94f0d4b689f5d69b3e5f58c87415c0
Aranfyro <141035777+Aranfyro@users.noreply.github.com>
2024-12-29 13:32:08 +0300

    Add files via upload

---
+++ module_11_3.py
@@ -0,0 +1,17 @@
+# Домашнее задание по теме "Интроспекция"
+from pprint import pprint
+
+def introspection_info(obj):
+    object_type = type(obj)
+    object_attributes =  [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
+    object_methods = [method for method in dir(obj) if callable(getattr(obj, method))]
+    object_module = obj.__class__.__module__ if hasattr(obj, '__class__') else 'built-in'
+    return {
+        'type': object_type,
+        'attributes': object_attributes,
+        'methods': object_methods,
+        'module': object_module,
+    }
+
+number_info = introspection_info(42)
+pprint(number_info)
